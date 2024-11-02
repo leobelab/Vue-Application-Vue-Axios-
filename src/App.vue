98290b248eb81b72397636a5cd57cf129467072c
leobelab <leobenitezzz77@gmail.com>
2024-11-02 19:36:57 +0100

    before_reaction_timer_3

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,60 @@
 <template>
   <h1>{{ title }}</h1>
-</template><!-- because there are different templates for different components
-                and we see some of them and not others at different moment -->
+  <p>Welcome...</p> 
+
+  <teleport to="#modals" v-if="showModal">
+      <Modal theme="sale" @close="toggleModal">
+        <template v-slot:links>
+          <a href="#">sign un now</a>
+          <a href="#">more info</a>
+        </template>
+        <h1>Ninja Giveaway!</h1>
+        <p>Grab your ninja sway for half price!</p>
+      </Modal>
+  </teleport>
+
+  <teleport to="#modals" v-if="showModalTwo">
+      <Modal @close="toggleModalTwo">
+        <h1>Sign up to the newsletter!</h1>
+        <p>For updates and promo codes!</p>
+      </Modal>
+  </teleport>
+
+  <button @click="toggleModal">open modal 1</button>
+  <button @click="toggleModalTwo">open modal 2</button>
+</template>
+<!-- because there are different templates for different components
+    and we see some of them and not others at different moment -->
 
 <script>
+import Modal from './components/Modal.vue'
+
 export default {
-  name: 'App',
+  name: 'App', //Component name
+  components: {
+    Modal
+  },
   data() {
     return {
-      title: 'My First Vu App :)'
+      title: 'My First Vue App :)',
+      showModal: false,
+      showModalTwo: false
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo
     }
   }
+  
 }
 </script>
 
 <style>
-#app {
+#app, #modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -29,3 +68,19 @@ h1 {
   padding-bottom: 10px;
 }
 </style>
+
+
+<!--
+<template>
+<input type="text" ref="name">
+  <button @click="handleClick">click me</button>
+<template>
+
+methods: {
+    handleClick() {
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    }
+  }
+  -->
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './assets/global.css'
 
 createApp(App).mount('#app')
 //It set de route component as App and template is injected on index.html and 
